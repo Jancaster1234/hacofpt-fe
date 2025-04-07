e1f68d89b00fd65ede8b5d0c7f0056aea642e60a
lqhuy28 <lequanghuy2809@gmail.com>
2025-04-08 01:11:14 +0700

    update file service

--- src/services/fileUrl.service.ts
+++ src/services/fileUrl.service.ts
@@ -1,9 +1,7 @@
 // src/services/fileUrl.service.ts
 import { FileUrl } from "@/types/entities/fileUrl";
 import { tokenService_v0 } from "@/services/token.service_v0";
-import { apiService } from "@/services/apiService_v0";
 class FileUrlService {
-
   async uploadFile(file: File): Promise<FileUrl> {
     const formData = new FormData();
     formData.append("file", file);
@@ -29,33 +27,76 @@ class FileUrlService {
     }
   }
 
-  async uploadMultipleFiles(files: File[]): Promise<FileUrl> {
+  async uploadMultipleFiles(files: File[]): Promise<FileUrl[]> {
     const formData = new FormData();
 
-    files.forEach(file => {
+    files.forEach((file) => {
       formData.append("files", file);
     });
 
     try {
-      const response = await fetch("/hackathon-service/api/v1/upload/multiple", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${tokenService_v0.getAccessToken()}`,  
-        },
-        body: formData,
-      });
+      const response = await fetch(
+        "/hackathon-service/api/v1/upload/multiple",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${tokenService_v0.getAccessToken()}`,
+          },
+          body: formData,
+        }
+      );
 
       if (!response.ok) {
         throw new Error(`Upload failed: ${response.statusText}`);
       }
 
-      const data = await response.json(); 
-      return data;
+      const data = await response.json();
+
+      if (data.code === 1000) {
+        return data
+      } else {
+        throw new Error("File upload failed, no data returned.");
+      }
+    } catch (error) {
+      console.error("Error uploading files:", error);
+      throw error;
+    }
+  }
+
+  async uploadMultipleFilesCommunication(files: File[]): Promise<FileUrl[]> {
+    const formData = new FormData();
+
+    files.forEach((file) => {
+      formData.append("files", file);
+    });
+
+    try {
+      const response = await fetch(
+        "/communication-service/api/v1/files/upload",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${tokenService_v0.getAccessToken()}`,
+          },
+          body: formData,
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(`Upload failed: ${response.statusText}`);
+      }
+
+      const data = await response.json();
+
+      if (data.code === 1000) {
+        return data
+      } else {
+        throw new Error("File upload failed, no data returned.");
+      }
     } catch (error) {
       console.error("Error uploading files:", error);
       throw error;
     }
   }
 }
-
 export const fileUrlService = new FileUrlService();
