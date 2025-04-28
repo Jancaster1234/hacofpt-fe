b1ae27b14b4ded0dd70db98b745e5b1fb31b59e4
Jancaster1234 <122957619+Jancaster1234@users.noreply.github.com>
2025-04-28 17:08:35 +0700

    feat: update menuData to remove translation function and use static titles for menu items

--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -1,43 +1,42 @@
 // src/components/Header/menuData.tsx
 import { Menu } from "@/types/menu";
 
-const menuData = (t: any): Menu[] => [
+const menuData: Menu[] = [
   {
     id: 1,
-    title: t("home"),
+    title: "Home",
     path: "/",
     newTab: false,
   },
   {
     id: 2,
-    title: t("hackathon"),
+    title: "Hackathon",
     path: "/hackathon",
     newTab: false,
   },
   {
     id: 3,
-    title: t("userDashboard"),
-    path: "/user-dashboard",
+    title: "User dashboard",
+    path: "/dashboard",
     newTab: false,
   },
   {
     id: 4,
-    title: t("chat"),
+    title: "Chat",
     path: "/chat",
     newTab: false,
   },
   {
     id: 5,
-    title: t("forum"),
+    title: "Forum",
     path: "/forum",
     newTab: false,
   },
   {
     id: 6,
-    title: t("help"),
+    title: "Help",
     path: "/help",
     newTab: false,
   },
 ];
-
 export default menuData;
